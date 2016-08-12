aa2c2c0ecbdb79d93194b2a9a068bff70a41a4a3
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-08-12 14:53:27 -0500

    created items model to search by username or keyword and return matching ads

---
+++ models/Items.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once __DIR__ . '/Model.php';
+
+class Items extends Model {
+
+    protected static $table = 'items';
+    public static function findAdsByUser($username)
+    {
+
+    	self::dbConnect();
+
+    	$query = 'SELECT * FROM ' . self::$table . ' WHERE username = :username';
+
+    	$stmt = self::$dbc->prepare($query);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $instances = [];
+        foreach ($results as $result) {
+            $instance = new static;
+            $instance->attributes = $results;
+            $instances[] = $instance;
+
+        }
+        return $instances;
+    }
+
+    public static function findByKeyword($keyword) {
+        self::dbConnect();
+
+        $query = 'SELECT * FROM ' .self::$table . ' WHERE keyword = :keyword';
+        $stmt = self::$dbc->prepare($query);
+        $stmt->bindValue(':keyword', $keyword PDO::PARAM_STR);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $instance = null;
+    }
+
+        $instances = [];
+        foreach ($results as $result) {
+            $instance = new static;
+            $instance->attributes = $results;
+            $instances[] = $instance;
+
+        }
+        return $instances;
+    }
+
+}
+
+?>
